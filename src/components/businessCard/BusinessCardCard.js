0e0638aa0560d1654ff9f48148a9bb7bfd81c8bd
jasonkim7288 <jason.kim7288@gmail.com>
2020-11-08 21:57:23 +1000

    change business card maker's component type

--- src/components/businessCard/BusinessCardCard.js
+++ src/components/businessCard/BusinessCardCard.js
@@ -1,5 +1,5 @@
 import { Box, Button } from '@material-ui/core';
-import React, { Component, createRef } from 'react'
+import React, { useRef, useState } from 'react'
 import html2canvas from 'html2canvas';
 import BusinessCardTemplate1 from './BusinessCardTemplate1';
 import BusinessCardTemplate2 from './BusinessCardTemplate2';
@@ -7,93 +7,89 @@ import BusinessCardSettings from './BusinessCardSettings';
 import BusinessCardImageSetting from './BusinessCardImageSetting';
 import Carousel from 'react-material-ui-carousel';
 
-export default class BusinessCardCard extends Component {
-  constructor(props) {
-    super(props);
-    this.savingAreaRef = createRef();
-    this.state = {
-      settings: {
-        textColor: '#d1d0d8',
-        backgroundColor: '#2a2b2e',
-        imgURL: process.env.PUBLIC_URL + '/coderAcademy.png'
-      }
-    }
-  }
+function BusinessCardCard({ submittedForm }) {
+  const [settings, setSettings] = useState({
+    textColor: '#d1d0d8',
+    backgroundColor: '#2a2b2e',
+    imgURL: process.env.PUBLIC_URL + '/coderAcademy.png'
+  });
+  const savingAreaRef = useRef(null);
 
-  openImage = (base64URL) => {
+  const openImage = (base64URL) => {
     var win = window.open();
     win.document.write(`<iframe src="` + base64URL  + `" frameborder="0" style="border:0;
    top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen>
    </iframe>`);
   }
 
-  handleClick = () => {
-    html2canvas(this.savingAreaRef.current, {
+  const handleClick = () => {
+    html2canvas(savingAreaRef.current, {
       scrollX: 0,
       scrollY: -window.scrollY,
       scale: 1,
-      backgroundColor: this.state.settings.backgroundColor
+      backgroundColor: settings.backgroundColor
     })
       .then(canvas => {
         const url = canvas.toDataURL();
         console.log('url:', url);
 
         // window.open(url, '_blank');
-        // this.openImage(url);
+        // openImage(url);
         const w = window.open('about:blank', 'image from canvas');
         w.document.write("<img src='" + url +"' alt='from canvas'/>")
       })
   }
 
-  updateSetting = (key, value) => {
-    this.setState({settings: {
-      ...this.state.settings,
+  const updateSetting = (key, value) => {
+    console.log('key, value:', key, value);
+    setSettings({
+      ...settings,
       [key]: value
-    }})
+    })
   }
 
-  render() {
-    const { textColor, backgroundColor } = this.state.settings;
-    const paperStyle = {
-      width: '1050px',
-      height: '600px',
-      minHeight: '600px',
-      maxHeight: '600px',
-      overflow: 'hidden'
-    }
-    const savingAreaStyle = {
-      margin: '0',
-      padding: '0',
-      backgroundColor,
-      color: textColor,
-      height: '100%',
-      width: '100%'
-    }
-    const cardTemplates = [
-      (<BusinessCardTemplate1 key="T1" settings={this.state.settings} submittedForm={this.props.submittedForm} />),
-      (<BusinessCardTemplate2 key="T2" settings={this.state.settings} submittedForm={this.props.submittedForm} />)
-    ]
+  const { textColor, backgroundColor } = settings;
+  const paperStyle = {
+    width: '1050px',
+    height: '600px',
+    minHeight: '600px',
+    maxHeight: '600px',
+    overflow: 'hidden'
+  }
+  const savingAreaStyle = {
+    margin: '0',
+    padding: '0',
+    backgroundColor,
+    color: textColor,
+    height: '100%',
+    width: '100%'
+  }
+  const cardTemplates = [
+    (<BusinessCardTemplate1 key="T1" settings={settings} submittedForm={submittedForm} />),
+    (<BusinessCardTemplate2 key="T2" settings={settings} submittedForm={submittedForm} />)
+  ]
 
-    return (
-      <div >
-        <div style={paperStyle}>
-          <div ref={this.savingAreaRef} style={savingAreaStyle}>
-            <Carousel navButtonsAlwaysVisible={false} autoPlay={false} interval="10000000" style={{margin: '0', padding: '0'}} >
-              {
-                cardTemplates.map(cardTemplate => cardTemplate)
-              }
-            </Carousel>
+  return (
+    <div >
+      <div style={paperStyle}>
+        <div ref={savingAreaRef} style={savingAreaStyle}>
+          <Carousel navButtonsAlwaysVisible={false} autoPlay={false} interval="10000000" style={{margin: '0', padding: '0'}} >
+            {
+              cardTemplates.map(cardTemplate => cardTemplate)
+            }
+          </Carousel>
 
-          </div>
         </div>
-        <Box my={5}>
-          <Box component="span" mr={3}>
-            <Button variant="contained" color="primary" onClick={this.handleClick}>Save as png</Button>
-          </Box>
-          <BusinessCardSettings settings={this.state.settings} updateSettingCb={this.updateSetting}/>
-          <BusinessCardImageSetting settings={this.state.settings} updateSettingCb={this.updateSetting}/>
-        </Box>
       </div>
-    )
-  }
+      <Box my={5}>
+        <Box component="span" mr={3}>
+          <Button variant="contained" color="primary" onClick={handleClick}>Save as png</Button>
+        </Box>
+        <BusinessCardSettings settings={settings} updateSettingCb={updateSetting}/>
+        <BusinessCardImageSetting settings={settings} updateSettingCb={updateSetting}/>
+      </Box>
+    </div>
+  )
 }
+
+export default BusinessCardCard
--- src/components/businessCard/BusinessCardMaker.js
+++ src/components/businessCard/BusinessCardMaker.js
@@ -1,34 +1,26 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Box } from '@material-ui/core';
 import BusinessCardForm from './BusinessCardForm';
 import BusinessCardCard from './BusinessCardCard';
 
-export default class BusinessCardMaker extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      submittedForm: null
-    }
+function BusinessCardMaker() {
+  const [submittedForm, setSubmittedForm] = useState(null);
+
+  const handleSubmitCb = (values) => {
+    setSubmittedForm(values)
   }
 
-  handleSubmitCb = (submittedForm) => {
-    console.log('handleSubmitCb');
-    this.setState({ submittedForm })
-  }
-
-  render() {
-    const { submittedForm } = this.state;
-    return (
-      <Box>
-        <BusinessCardForm handleSubmitCb={this.handleSubmitCb}/>
-        { submittedForm &&
-          (
-            <BusinessCardCard submittedForm={submittedForm}/>
-          )
-        }
-        <br />
-
-      </Box>
-    )
-  }
+  return (
+    <Box>
+      <BusinessCardForm handleSubmitCb={handleSubmitCb}/>
+      { submittedForm &&
+        (
+          <BusinessCardCard submittedForm={submittedForm}/>
+        )
+      }
+      <br />
+    </Box>
+  )
 }
+
+export default BusinessCardMaker
